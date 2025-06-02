3b96b93812b1fb9659d63fa9c21f95285ace448f
Ben <benpack007@gmail.com>
2025-06-02 11:38:43 -0700

    initial setup

--- healthy-or-die/src/App.tsx
+++ healthy-or-die/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Snooze from './Snooze.jsx'
+import VideoPlayer from './Video.jsx'
+import ThreatLevel from './ThreatLevel.jsx'
+import StopButton from './StopButton.jsx'
 
 function App() {
+
   const [count, setCount] = useState(0)
+  const [threatLevel, setThreatLevel] = useState(10)
+  const [waterDrank, setWaterDrank] = useState(false)
+  const [snooze, setSnooze] = useState(false)
+
+  const resetThreatLevel = () => {
+    setThreatLevel(0)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="gap-4 grid grid-cols-[65%_35%] border-emerald-400 h-full w-full">
+        <div className="">
+          <VideoPlayer />
+          <StopButton />
+        </div>
+        <div className="">
+          <ThreatLevel threatLevel={threatLevel} resetThreatLevel={resetThreatLevel} />
+          <Snooze />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ healthy-or-die/src/StopButton.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function StopButton( {resetThreatLevel} ) {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button class="w-100 h-40 border-2 border-blue-900" onClick={() => resetThreatLevel()}>Stop</button>
+    </>
+  )
+}
+
+export default StopButton
