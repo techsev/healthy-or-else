c4d40954c4721f140eb3d939b54cd17dd47f157e
TECHAWAY\techs <techsev@gmail.com>
2025-06-02 14:50:15 -0700

    test

---
+++ healthy-or-die/src/Timer2.jsx
@@ -0,0 +1,37 @@
+
+import { useState, useEffect, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Timer( {barRate, setBarRate, barValue, setBarValue, triggerEffect, increaseThreatLevel} ) {
+  const [count, setCount] = useState(0)
+  const [setting, setSetting] = useState(false)
+  const barValueRef = useRef(barValue)
+
+  useEffect (() => {
+    const interval = setInterval(() => {
+      if (barValueRef.current >= 1) {
+        barValueRef.current = 0
+        triggerEffect()
+        increaseThreatLevel()
+      } else {
+        barValueRef.current = barValueRef.current + .0008
+      }
+    }, barRate)
+  }, []);
+
+
+  
+
+  return (
+    <>
+      <div className="ml-28 w-100 h-30 flex flex-col">
+        Thirst Meter
+        <progress className="w-100 h-15 bg-blue-600 rotate-180" value={barValue} onClick={() => setBarValue(0)}></progress>
+      </div>
+    </>
+  )
+}
+
+export default Timer
